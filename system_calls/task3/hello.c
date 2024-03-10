4a3aaa7a5009d80b2dc044f33f7328995d92d474
reVInotip <grigoriy.novikov220@gmail.com>
2024-03-10 19:19:22 +0700

    a

---
+++ system_calls/task3/hello.c
@@ -0,0 +1,13 @@
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+
+int main(void) {
+    int countWriteenBytes = syscall(SYS_write, 1, "Hello world!", 12);
+    if (countWriteenBytes != 12) {
+        fprintf(stderr, "Something went wrong!, ret=%d: %s", countWriteenBytes, strerror(errno));
+    }
+    return 0;
+}
